3210e025e45446fc98194511856e12e601d3abc3
dinero <dinarsabangulov@192.168.1.100>
2025-05-03 20:12:34 +0300

    Start project

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const {v4: uuid} = require('uuid');
+
+class Lib {
+    constructor(
+        id = uuid(),
+        title = '',
+        description = '',
+        authors = '',
+        favourite = '',
+        fileCover = '',
+        fileName = '') {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.authors = authors;
+    this.favourite = favourite;
+    this.fileCover = fileCover;
+    this.fileName = fileName;
+    }
+}
+
+const library = {
+    'books': []
+}
+
+const app = express();
+app.use(express.json());
+
+app.post('/api/user/login', (req, res) => {
+
+})
+
+app.get('/api/books', (req, res) => {
+
+})
+
+app.get('/api/books/:id', (req, res) => {
+
+})
+
+app.post('/api/books', (req, res) => {
+
+})
+
+app.put('/api/books/:id', (req, res) => {
+
+})
+
+app.delete('/api/books/:id', (req, res) => {
+
+})
